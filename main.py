a30dbc0cbc9f217a889493651d51ed01c0994b70
Trevor DePew <t.depewmusic@gmail.com>
2023-05-11 18:44:46 -0400

    added ball and player paddle animations

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 import pygame, sys
 
+
+def ball_animation():
+    global ball_speed_x, ball_speed_y #really only a good solution for small programs - for larger applications use return statements or classes
+    ball.x += ball_speed_x
+    ball.y += ball_speed_y
+
+    #Use >= or <=, not == - if ball is at 955px and the next frame it is at 962px, it will 'bounce' instead of continuing because it never reaches 960px
+    if ball.top <= 0 or ball.bottom >= screen_height:
+        ball_speed_y *= -1
+    if ball.left <= 0 or ball.right >= screen_width:
+        ball_speed_x *= -1
+
+    if ball.colliderect(player) or ball.colliderect(opponent):
+        ball_speed_x *= -1
+    
+
 # General setup
 pygame.init()
 clock = pygame.time.Clock()
@@ -18,12 +34,30 @@ opponent = pygame.Rect(10, screen_height/2 - 70, 10, 140)
 bg_color = pygame.Color('grey12')
 light_grey = (200,200,200)
 
+ball_speed_x = 7
+ball_speed_y = 7
+player_speed = 0
+
 while True:
     # Handling input
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_DOWN:
+                player_speed += 7
+            if event.key == pygame.K_UP:
+                player_speed -= 7
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_DOWN:
+                player_speed -=7
+            if event.key == pygame.K_UP:
+                player_speed += 7
+
+
+    ball_animation()
+    player.y += player_speed
 
     #Visuals
     #Beware of order first element drawn at bottom of the frame, last eliment at the top (z-index)
@@ -35,6 +69,7 @@ while True:
     pygame.draw.ellipse(screen, light_grey, ball)
     #anti-alias line(surface to draw on, color, tuple with start point, tuple with end point)
     pygame.draw.aaline(screen, light_grey, (screen_width/2,0), (screen_width/2,screen_height))
+
     # Updating the window
     pygame.display.flip()
     clock.tick(60)
