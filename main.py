bef6f10dfc5cf1eb4c15b99a988d5a34b7c593d9
Trevor DePew <t.depewmusic@gmail.com>
2023-05-12 02:08:12 -0400

    added score display and logic

--- main.py
+++ main.py
@@ -2,14 +2,19 @@ import pygame, sys, random
 
 
 def ball_animation():
-    global ball_speed_x, ball_speed_y #really only a good solution for small programs - for larger applications use return statements or classes
+    global ball_speed_x, ball_speed_y, player_score, opponent_score #really only a good solution for small programs - for larger applications use return statements or classes
     ball.x += ball_speed_x
     ball.y += ball_speed_y
 
     #Use >= or <=, not == - if ball is at 955px and the next frame it is at 962px, it will 'bounce' instead of continuing because it never reaches 960px
     if ball.top <= 0 or ball.bottom >= screen_height:
         ball_speed_y *= -1
-    if ball.left <= 0 or ball.right >= screen_width:
+    if ball.left <= 0:
+        player_score += 1
+        ball_restart()
+    
+    if ball.right >= screen_width:
+        opponent_score += 1
         ball_restart()
 
     if ball.colliderect(player) or ball.colliderect(opponent):
@@ -27,6 +32,7 @@ def opponent_ai():
         opponent.top += opponent_speed
     if opponent.bottom > ball.y:
         opponent.bottom -= opponent_speed
+
     if opponent.top <= 0:
         opponent.top = 0
     if opponent.bottom >= screen_height:
@@ -54,13 +60,20 @@ ball = pygame.Rect(screen_width/2 - 15, screen_height/2 -15, 30, 30)
 player = pygame.Rect(screen_width - 20, screen_height/2 - 70, 10,140)
 opponent = pygame.Rect(10, screen_height/2 - 70, 10, 140)
 
+#Colors
 bg_color = pygame.Color('grey12')
 light_grey = (200,200,200)
 
+#Game Variables
 ball_speed_x = 7 * random.choice((1,-1))
 ball_speed_y = 7
 player_speed = 0
 opponent_speed = 7
+
+#Text Variables
+player_score = 0
+opponent_score = 0
+game_font = pygame.font.Font("freesansbold.ttf", 32)
 
 while True:
     # Handling input
@@ -81,7 +94,7 @@ while True:
             if event.key == pygame.K_UP:
                 player_speed += 7
 
-
+    #Game Logic
     ball_animation()
     player_animation()
     opponent_ai()
@@ -97,6 +110,12 @@ while True:
     pygame.draw.ellipse(screen, light_grey, ball)
     #anti-alias line(surface to draw on, color, tuple with start point, tuple with end point)
     pygame.draw.aaline(screen, light_grey, (screen_width/2,0), (screen_width/2,screen_height))
+
+    player_text = game_font.render(f"{player_score}", False, light_grey)
+    screen.blit(player_text, (660,470))
+
+    opponent_text = game_font.render(f"{opponent_score}", False, light_grey)
+    screen.blit(opponent_text, (600, 470))
 
     # Updating the window
     pygame.display.flip()
